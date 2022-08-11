another attempt on fixing mmse power normalization

source/python/detectors.py
@@ -39,7 +39,8 @@ class LinearMmse(Detector):
     def detect(self, data_input, channel, constellation, snr): #noiseVar):
         f1 = np.matmul(np.transpose(np.conj(channel)), channel)
         #f2 = 2*noiseVar*np.identity(channel.shape[1])
-        f2 = (channel.shape[1]/tolin(snr))*np.identity(channel.shape[1])
+        #f2 = (channel.shape[1]/tolin(snr))*np.identity(channel.shape[1])
+        f2 = (2*channel.shape[0]/channel.shape[1]*tolin(-1*snr))*np.identity(channel.shape[1])
 
         channel_transf = np.matmul(np.linalg.pinv(f1+f2),np.transpose(np.conj(channel)))
 